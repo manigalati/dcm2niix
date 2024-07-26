2e495a3292132618ddd9f8aee5a4d4d788e8b634
manigalati <32110860+manigalati@users.noreply.github.com>
2024-07-26 16:59:56 +0200

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
 import argparse
+import nibabel as nib
+import numpy as np
 
 class DirectoryError(Exception):
     pass
 
-def run_dcm2niix(dir_in, dir_out):
-    command = "dcm2niix -z y -o '{}' '{}'".format(dir_out, dir_in)
+def run_dcm2niix(dir_in, dir_out, extra_options):
+    command = "dcm2niix {} -o '{}' '{}'".format(extra_options, dir_out, dir_in)
     subprocess.check_call(command, shell=True)
 
 def delete_unmatched_files(dir_out, keywords):
@@ -17,13 +19,53 @@ def delete_unmatched_files(dir_out, keywords):
         if all([
             os.path.isfile(file_path),
             file_name.endswith(".nii.gz"),
-            any([k in file_name for k in keywords]),
+            any([k.lower() in file_name.lower() for k in keywords]),
         ]):
             continue
         
         os.remove(file_path)
 
-def main(dir_in, dir_out, keywords):        
+def process_swi(dir_out, print_shape=False):
+    images_ph = [f for f in os.listdir(dir_out) if "swi" in f.lower() and f.endswith("ph.nii.gz")]
+    images_mg = [f for f in os.listdir(dir_out) if "swi" in f.lower() and not f.endswith("ph.nii.gz")]
+    
+    if len(images_ph) == 0 or len(images_mg) == 0:
+        print("SWI undetected")
+        exit(1)
+    
+    image_ph = np.zeros([0,0,0])
+    for f in images_ph:
+        f = nib.load(os.path.join(dir_out, f))
+        f, affine, header = f.get_fdata(), f.affine, f.header
+        if f.shape[-1] > image_ph.shape[-1]:
+            image_ph = f
+            image_ph_aff = affine
+            image_ph_header = header
+        elif f.shape[-1] == image_ph.shape[-1]:
+            image_ph += f
+    image_ph /= len(images_ph)
+    
+    image_mg = np.zeros([0,0,0])
+    for f in images_mg:
+        f = nib.load(os.path.join(dir_out, f))
+        f, affine, header = f.get_fdata(), f.affine, f.header
+        if f.shape[-1] > image_mg.shape[-1]:
+            image_mg = f
+            image_mg_aff = affine
+            image_mg_header = header
+        elif f.shape[-1] == image_mg.shape[-1]:
+            image_mg += f
+    image_mg /= len(images_mg)
+    
+    image = image_mg * image_ph**4
+    
+    nib.save(
+        nib.Nifti1Image(image.astype(float), image_mg_aff, image_mg_header),
+        os.path.join(dir_out, "swi.nii.gz")
+    )
+    
+
+def main(dir_in, dir_out, keywords, extra_options):        
     #The input directory must exist
     assert os.path.isdir(dir_in), "The input directory {} does not exist.".format(dir_in)
     
@@ -32,7 +74,7 @@ def main(dir_in, dir_out, keywords):
     
     #dcm2niix is executed
     try:
-        run_dcm2niix(dir_in, dir_out)
+        run_dcm2niix(dir_in, dir_out, extra_options)
     except subprocess.CalledProcessError as e:
         print("Error running dcm2niix: {}".format(e))
         exit(1)
@@ -41,13 +83,17 @@ def main(dir_in, dir_out, keywords):
         exit(1)
     
     delete_unmatched_files(dir_out, keywords)
+    
+    if any([k.lower() == "swi" for k in keywords]):
+        process_swi(dir_out, print_shape = "PRINTTT" in extra_options)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert DICOM to NIfTI and filter output files by keywords.")
     parser.add_argument('dir_in', type=str, help='Input directory containing DICOM files')
     parser.add_argument('dir_out', type=str, help='Output directory to store NIfTI files')
-    parser.add_argument('keywords', nargs='+', help='List of keywords to filter output files')
+    parser.add_argument('--keywords', nargs='+', default=[""], help='List of keywords to filter output files')
+    parser.add_argument('--extra-options', type=str, default='-z y -m y', help='Additional options to pass to the conversion process')
 
     args = parser.parse_args()
     
-    main(args.dir_in, args.dir_out, args.keywords)
+    main(args.dir_in, args.dir_out, args.keywords, args.extra_options)
